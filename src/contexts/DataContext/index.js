92e4fcfa125df4dee6dd663155526c8e58e89f35
Estelle Fouqueteau <estelle.fqt29@outlook.fr>
2025-04-08 16:40:19 +0200

    correction preview footer

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,24 +19,37 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [last, setLast] = useState(null); // Ajout d'un state pour 'last'
+
   const getData = useCallback(async () => {
     try {
-      setData(await api.loadData());
+      const loadedData = await api.loadData();
+
+      setData(loadedData);
+
+      // Calculer la dernière prestation
+      const sortedEvents = loadedData.events
+        .map((event) => ({ ...event, date: new Date(event.date) })) // Convertir en date
+        .sort((a, b) => b.date - a.date); // Trier du plus récent au plus ancien
+
+      setLast(sortedEvents[0]); // Dernière prestation
     } catch (err) {
       setError(err);
     }
   }, []);
+
   useEffect(() => {
     if (data) return;
     getData();
-  });
-  
+  }, [data, getData]);
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last, // ajout de last dans le contexte
       }}
     >
       {children}
@@ -46,7 +59,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
